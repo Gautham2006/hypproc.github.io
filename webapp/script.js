f0ecb07ec928c6a2059983c7d696263526b5cbba
Sriram NC <prajna4medev@jetusa.org>
2024-07-25 21:52:17 +0530

    h

--- webapp/script.js
+++ webapp/script.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
         window.location.href = authUrl;
     });
 
-    get started
+    //get started
     document.getElementById('getstarted').addEventListener('click', () => {
         console.log('get started button clicked');
         const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&scope=${encodeURIComponent(scope)}`;
